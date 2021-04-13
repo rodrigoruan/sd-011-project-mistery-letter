85ed6970d7784f180c6d73190fd7505c8cd56988
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-13 19:18:15 -0300

    corrige requisito 3

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+function clearLetter() {
+  const wordList = document.querySelectorAll('.word');
+  const paragraph = document.getElementById('carta-gerada');
+  for (let index = 0; index < wordList.length; index += 1) {
+    paragraph.removeChild(wordList[index]);
+  }
+}
+
 function generateLetter() {
   const input = document.getElementById('carta-texto');
   console.log(input.value);
   const letter = input.value.split(' ');
   console.log(letter);
+  clearLetter();
   const parent = document.getElementById('carta-gerada');
   if (letter[0] === '') {
     parent.innerText = 'Por favor, digite o conteúdo da carta.';
