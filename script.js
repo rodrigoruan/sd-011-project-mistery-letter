5ba0f7de8b095b6a5587f80a937a1da801ad9633
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-13 19:01:39 -0300

    implementa requisito 4

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function generateLetter() {
   const input = document.getElementById('carta-texto');
-  console.log(input);
+  console.log(input.value);
   const letter = input.value.split(' ');
+  console.log(letter);
   const parent = document.getElementById('carta-gerada');
+  if (letter[0] === '') {
+    parent.innerText = 'Por favor, digite o conteúdo da carta.';
+    return;
+  }
   for (let index = 0; index < letter.length; index += 1) {
     const element = document.createElement('span');
     element.innerText = letter[index];
