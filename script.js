fc5e9e8561a47cda217cf922755a833cfb1ffa08
Víctor Faria <vitilevi@hotmail.com>
2021-04-13 17:35:25 -0300

    Feito requisito 18

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 // Retrieve elements
 const generateButton = document.getElementById('criar-carta');
 const paragraph = document.getElementById('carta-gerada');
+const arrayOfClasses = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+
 
 function checkForInput(value) {
   if (value === '' || value === ' ') {
     const createErrorElement = document.createElement('span');
     createErrorElement.innerText = 'Por favor, digite o conteúdo da carta.';
     paragraph.appendChild(createErrorElement);
-  }
+  } else {
+    return true;
+  }  
+}
+
+function generateRandomClass (array) {
+  const arrayInt = array;
+  const randomNumber = Math.floor(Math.random() * arrayInt.length);
+  const randomClass = arrayInt[randomNumber];
+  return randomClass;
 }
 
 function removeAllContent() {
@@ -16,21 +27,53 @@ function removeAllContent() {
   }
 }
 
+function changeWordStyle(event) {
+  const newClassName = generateRandomClass(arrayOfClasses);
+  event.target.className = '';
+  event.target.classList.add(newClassName);
+}
+
+function updateSpan() {
+  const spanTag = document.getElementsByTagName('span');
+  for (let index = 0; index < spanTag.length; index += 1) {
+    spanTag[index].addEventListener('click', changeWordStyle);
+  }
+  return spanTag;
+}
+
+function createWordCounter(value, inputValue) {
+  const tagUpdate = value;
+  const input = inputValue;
+  if (input === '' || input === ' ') {
+    return;
+  } else {
+    const counter = document.createElement('p');
+    counter.id = 'carta-contador';
+    counter.innerText = `${tagUpdate.length}`
+    paragraph.appendChild(counter);
+  }
+}
+
 function createWord() {
   const documentInput = document.getElementById('carta-texto');
   const textInput = documentInput.value;
   removeAllContent();
-  checkForInput(textInput);
-  const textArray = textInput.split(' ');
-  for (let index = 0; index < textArray.length; index += 1) {
-    const createSpan = document.createElement('span');
-    createSpan.innerText = textArray[index];
-    paragraph.appendChild(createSpan);
+  if (checkForInput(textInput)) {
+    const textArray = textInput.split(' ');
+    for (let index = 0; index < textArray.length; index += 1) {
+      const createSpan = document.createElement('span');
+      const randomGeneratedClass = generateRandomClass(arrayOfClasses);
+      createSpan.innerText = `${textArray[index]}`;
+      createSpan.classList.add(randomGeneratedClass);
+      paragraph.appendChild(createSpan);
+    }
+    const update = updateSpan();
+    createWordCounter(update, textInput);
   }
 }
 
 function createEventListeners() {
-  generateButton.addEventListener('click', createWord);
+  generateButton.addEventListener('click', createWord);  
 }
 
 function init() {
