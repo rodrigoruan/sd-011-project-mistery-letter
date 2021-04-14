592c0cde1a30e16302014d321662a271feaf263b
IsleneGomes <islenegomes@gmail.com>
2021-04-14 19:56:42 -0300

    Requisito 4.1

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const input = document.getElementById('carta-texto');
 const botao = document.getElementById('criar-carta');
 const paragrafo = document.getElementById('carta-gerada');
+let span = document.createElement('span');
+paragrafo.appendChild(span);
 
+input.addEventListener('click', () => {
+  if (input.value !== 0) {
+    span = input.value.split(' ');
+    for (let index = 1; index < span.length; index += 1) {
+      paragrafo.innerHTML += span[index];
+    }
+  } else {
+  paragrafo.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+});
