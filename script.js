f79333127fa21e4dc3726ce2d9ebadb3120c2bf1
IsleneGomes <islenegomes@gmail.com>
2021-04-14 19:18:42 -0300

    Requisito 8.6

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const input = document.getElementById('carta-texto');
-let span = document.createElement('span');
-span = input.value;
 const botao = document.getElementById('criar-carta');
 const paragrafo = document.getElementById('carta-gerada');
+
+input.addEventListener('click', () => {
+  if (input.value !== 0) {
+    const span = input.value.split(' ');
+    paragrafo.innerHTML = `<span>${span[0]}<span>`;
+    for (let index = 1; index < span.length; index += 1) {
+      paragrafo.innerHTML += `<span>${span[index]}<span>`;
+    }
+  }
+});
