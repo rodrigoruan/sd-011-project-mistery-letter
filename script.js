d6fc49d4546f53433cd1dba9510d86130f20efda
IsleneGomes <islenegomes@gmail.com>
2021-04-14 13:55:27 -0300

    Requisito 8.4

--- script.js
+++ script.js
@@ -4,15 +4,3 @@ let span = document.createElement('span');
 span = input.value;
 const botao = document.getElementById('criar-carta');
 const paragrafo = document.getElementById('carta-gerada');
-
-function criarItensSpans() {
-let arrayInput = span.split(' ');
-for (let index = 0; index < arrayInput.length; index += 1) {
-  if (span !== '') {
-      span[index] = input.appendChild(document.createTextNode(span));
-    } else {
-      paragrafo.innerHTML = 'Por favor, digite o conteúdo da carta.'
-    }
-  }
-}
-botao.addEventListener('click', criarItensSpans)
