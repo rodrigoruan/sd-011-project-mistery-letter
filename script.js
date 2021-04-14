132bde838bf630eea2d84953f851b32975627f66
IsleneGomes <islenegomes@gmail.com>
2021-04-14 13:49:23 -0300

    Requisito 8.3

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function tags(elemento) {
-  document.createElement(elemento);
-}
 
-let frase = "Eu quero escrever uma carta muito longa";
-let palavras = frase.split(' ');
+const input = document.getElementById('carta-texto');
+let span = document.createElement('span');
+span = input.value;
+const botao = document.getElementById('criar-carta');
+const paragrafo = document.getElementById('carta-gerada');
+
+function criarItensSpans() {
+let arrayInput = span.split(' ');
+for (let index = 0; index < arrayInput.length; index += 1) {
+  if (span !== '') {
+      span[index] = input.appendChild(document.createTextNode(span));
+    } else {
+      paragrafo.innerHTML = 'Por favor, digite o conteúdo da carta.'
+    }
+  }
+}
+botao.addEventListener('click', criarItensSpans)
