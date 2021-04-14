a1a88495960877e84b116d2e9c2131feecffc70c
IsleneGomes <islenegomes@gmail.com>
2021-04-14 19:25:58 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ const input = document.getElementById('carta-texto');
 const botao = document.getElementById('criar-carta');
 const paragrafo = document.getElementById('carta-gerada');
 
-input.addEventListener('click', () => {
-  if (input.value !== 0) {
-    const span = input.value.split(' ');
-    paragrafo.innerHTML = `<span>${span[0]}<span>`;
-    for (let index = 1; index < span.length; index += 1) {
-      paragrafo.innerHTML += `<span>${span[index]}<span>`;
-    }
-  }
-});
