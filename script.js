9d6b04ba96919d4ab02b2b9ac77488676dda2e52
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-14 09:24:37 -0300

    Requisito 3: testes

---
+++ script.js
@@ -0,0 +1,18 @@
+// Requisito 3: Quebrar frase e criar spans
+const generateButton = document.querySelector('#criar-carta');
+const originalText = document.querySelector('#carta-texto');
+const allSpans = document.querySelector('#carta-gerada');
+
+function createSpans() {
+  generateButton.addEventListener('click', () => {
+    const string = originalText.value;
+    const array = string.split(' ');
+    console.log(array);
+    for (let index = 0; index < array.length; index += 1) {
+      const span = document.createElement('span');
+      span.innerText = array[index];
+      allSpans.appendChild(span);
+    }
+  });
+}
+createSpans();
