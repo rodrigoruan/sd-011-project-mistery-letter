7d1104f20c4960071cfa6c4e32288b48c24a3a7a
Tales Coelho <tales-coelho@live.com>
2021-04-14 12:14:02 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -3,20 +3,14 @@ const cartaTexto = document.getElementById('carta-texto');
 const cartaGerada = document.getElementById('carta-gerada');
 
 criarCarta.addEventListener('click', () => {
-  if (cartaTexto.value) {
+  if (cartaTexto.value && cartaTexto.value !== ' ') {
     const texts = cartaTexto.value.split(' ');
     console.log(texts);
     cartaGerada.innerHTML = `<span>${texts[0]}<span>`;
     for (let index = 1; index < texts.length; index += 1) {
       cartaGerada.innerHTML += `<span>${texts[index]}<span>`;
     }
+  } else {
+    cartaGerada.innerText = 'por favor, digite o conteúdo da carta.';
   }
 });
-
-// 3 - Deve haver um botão com id="criar-carta" e ao clicar nesse botão, a carta misteriosa deve ser gerada
-// Pontos importantes:
-// * Cada palavra deve aparecer dentro de uma tag `span`.
-// * As tags `span` devem ser adicionadas como filhas do parágrafo que possui o id `carta-gerada`.
-// O que será verificado:
-// Será validado que existe um elemento button com id="criar-carta"
-// Será validado que ao clicar no botão, a carta misteriosa deve ser gerada
