7b844f22113f7c19074fee01699b8c9176ec0210
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-15 08:18:23 -0300

    Requisito 16

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-// Requisito 3: Quebrar frase e criar spans
 const generateButton = document.querySelector('#criar-carta');
 const originalText = document.querySelector('#carta-texto');
 const allSpans = document.querySelector('#carta-gerada');
+
+// Criação de grupos de classes
+const spanStyle = ['newspaper', 'magazine1', 'magazine2'];
+const spanSize = ['medium', 'big', 'reallybig'];
+const spanRotation = ['rotateleft', 'rotateright'];
+const spanWay = ['skewleft', 'skewright'];
+
+// função para alocar classes
+function sortClasses() {
+  const span = document.querySelectorAll('span');
+  for (let index = 0; index < span.length; index += 1) {
+    let classIndex = Math.floor(Math.random() * spanStyle.length);
+    span[index].classList.add(`${spanStyle[classIndex]}`);
+    classIndex = Math.floor(Math.random() * spanSize.length);
+    span[index].classList.add(`${spanSize[classIndex]}`);
+    classIndex = Math.floor(Math.random() * spanRotation.length);
+    span[index].classList.add(`${spanRotation[classIndex]}`);
+    classIndex = Math.floor(Math.random() * spanWay.length);
+    span[index].classList.add(`${spanWay[classIndex]}`);
+  }
+}
 
 function createSpans() {
   generateButton.addEventListener('click', () => {
@@ -16,6 +36,7 @@ function createSpans() {
         span.innerText = array[index];
         allSpans.appendChild(span);
       }
+      sortClasses();
     }
   });
 }
