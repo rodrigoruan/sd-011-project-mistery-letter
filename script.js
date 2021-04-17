ac924bbb75ab5e26301ac5242b65b3ffbcba630d
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-16 22:10:54 -0300

    Passo 3

---
+++ script.js
@@ -0,0 +1,17 @@
+function createLetter() {
+  const button = document.getElementById('criar-carta');
+  const input = document.getElementById('carta-texto');
+  const result = document.getElementById('carta-gerada');
+
+  button.addEventListener('click', () => {  
+    const breakInput = input.value.match(/\b(\w+)\b/g);
+    
+    for (let i = 0; i < breakInput.length; i += 1) {
+      const span = document.createElement('span');
+      span.innerText = breakInput[i];
+      result.appendChild(span);
+    }
+  })
+}
+
+createLetter()
