4415bac6d5ed93438def8467fa30058ecb2a2762
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-16 23:08:45 -0300

    Tentando passo 3 dnv

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+const button = document.getElementById('criar-carta');
+const input = document.getElementById('carta-texto');
+const result = document.getElementById('carta-gerada');
+
+const createLetter = () => {
+  const separateWords = input.value.split(' ');
+  separateWords.forEach((palavra) => {
+    const span = document.createElement('span');
+    span.innerText = palavra + ' ';
+    result.append(span);
+  });
+};
+
+button.addEventListener('click', createLetter);
+
+/*
 function createLetter() {
   const button = document.getElementById('criar-carta');
   const input = document.getElementById('carta-texto');
   const result = document.getElementById('carta-gerada');
-  
+
   button.addEventListener('click', () => {
-  const breakInput = input.value.match(/\b(\w+)\b/g);
+    const breakInput = input.value.match(/\b(\w+)\b/g);
 
     for (let i = 0; i < breakInput.length; i += 1) {
       const span = document.createElement('span');
@@ -15,3 +31,4 @@ function createLetter() {
 }
 
 createLetter();
+*/
