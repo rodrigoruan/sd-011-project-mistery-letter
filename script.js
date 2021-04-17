1f689a561ef6ae1e39400e23fbe6dbde4658bd60
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-16 23:28:27 -0300

    Passo 5

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
-const button = document.getElementById('criar-carta');
-const input = document.getElementById('carta-texto');
-const result = document.getElementById('carta-gerada');
-
-const createLetter = () => {
-  const separateWords = input.value.split(' ');
-  separateWords.forEach((palavra) => {
-    const span = document.createElement('span');
-    span.innerText = palavra + ' ';
-    result.append(span);
-  });
-};
-
-button.addEventListener('click', createLetter);
-
-/*
 function createLetter() {
   const button = document.getElementById('criar-carta');
   const input = document.getElementById('carta-texto');
   const result = document.getElementById('carta-gerada');
 
   button.addEventListener('click', () => {
-    const breakInput = input.value.match(/\b(\w+)\b/g);
+    result.innerText = '';
+    
+    if (!input.value || !input.value.trim()) {
+      result.innerText = 'Por favor, digite o conteúdo da carta';
+    } else {
+      const breakInput = input.value.match(/\b(\w+)\b/g);
 
-    for (let i = 0; i < breakInput.length; i += 1) {
-      const span = document.createElement('span');
-      span.innerText = breakInput[i];
-      result.appendChild(span);
+      for (let i = 0; i < breakInput.length; i += 1) {
+        const span = document.createElement('span');
+        span.innerText = breakInput[i];
+        result.appendChild(span);
+      }
     }
   });
 }
 
 createLetter();
-*/
