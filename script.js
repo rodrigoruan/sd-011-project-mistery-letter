afbe9927286673ac6322cdc92ec7eb43b5b071c9
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-17 00:56:51 -0300

    Passo 16

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+const sizeGroup = ['medium', 'big', 'reallybig'];
+const rotationGroup = ['rotateleft', 'rotateright'];
+const inclinationGroup = ['skewleft', 'skewright'];
+
+function randomClass() {
+  const randomStyle = Math.floor(Math.random() * styleGroup.length);
+  const randomSize = Math.floor(Math.random() * sizeGroup.length);
+  const randomRotation = Math.floor(Math.random() * rotationGroup.length);
+  const randomInclination = Math.floor(Math.random() * inclinationGroup.length);
+  const span = document.getElementsByTagName('span');
+
+  for (let i = 0; i < span.length; i += 1) {
+    span[i].classList.add(styleGroup[randomStyle]);
+    span[i].classList.add(sizeGroup[randomSize]);
+    span[i].classList.add(rotationGroup[randomRotation]);
+    span[i].classList.add(inclinationGroup[randomInclination]);
+  }
+}
+
 function createLetter() {
   const button = document.getElementById('criar-carta');
   const input = document.getElementById('carta-texto');
@@ -14,6 +34,7 @@ function createLetter() {
         const span = document.createElement('span');
         span.innerText = breakInput[i];
         result.appendChild(span);
+        randomClass();
       }
     }
   });
