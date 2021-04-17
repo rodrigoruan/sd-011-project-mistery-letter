89b326744d6ff0e886132bbe18f20c0a8938bca7
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-17 01:45:45 -0300

    Passo 17

--- script.js
+++ script.js
@@ -2,27 +2,46 @@ const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
 const sizeGroup = ['medium', 'big', 'reallybig'];
 const rotationGroup = ['rotateleft', 'rotateright'];
 const inclinationGroup = ['skewleft', 'skewright'];
+const button = document.getElementById('criar-carta');
+const input = document.getElementById('carta-texto');
+const result = document.getElementById('carta-gerada');
+const span = document.getElementsByTagName('span');
+
+function random(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return randomIndex;
+}
+
+function checkRepeat() {
+  for (let i = 0; i < span.length; i += 1) {
+    for (let j = 1; j < span.length; j += 1) {
+      if (span[i].classList === span[j].classList) {
+        randomClass();
+      }
+    }
+  }
+}
 
 function randomClass() {
-  const randomStyle = Math.floor(Math.random() * styleGroup.length);
-  const randomSize = Math.floor(Math.random() * sizeGroup.length);
-  const randomRotation = Math.floor(Math.random() * rotationGroup.length);
-  const randomInclination = Math.floor(Math.random() * inclinationGroup.length);
-  const span = document.getElementsByTagName('span');
-
   for (let i = 0; i < span.length; i += 1) {
-    span[i].classList.add(styleGroup[randomStyle]);
-    span[i].classList.add(sizeGroup[randomSize]);
-    span[i].classList.add(rotationGroup[randomRotation]);
-    span[i].classList.add(inclinationGroup[randomInclination]);
+    span[i].className = '';
+    span[i].classList.add(styleGroup[random(styleGroup)]);
+    span[i].classList.add(sizeGroup[random(sizeGroup)]);
+    span[i].classList.add(rotationGroup[random(rotationGroup)]);
+    span[i].classList.add(inclinationGroup[random(inclinationGroup)]);
+  }
+}
+
+function clickRandom() {
+  for (let i = 0; i < span.length; i += 1) {
+    span[i].addEventListener('click', () => {
+      randomClass();
+      checkRepeat();
+    });
   }
 }
 
 function createLetter() {
-  const button = document.getElementById('criar-carta');
-  const input = document.getElementById('carta-texto');
-  const result = document.getElementById('carta-gerada');
-
   button.addEventListener('click', () => {
     result.innerText = '';
 
@@ -35,6 +54,8 @@ function createLetter() {
         span.innerText = breakInput[i];
         result.appendChild(span);
         randomClass();
+        checkRepeat();
+        clickRandom();
       }
     }
   });
