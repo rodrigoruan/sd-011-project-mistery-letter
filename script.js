918e8fba00eb03c566b461dbd8ef2262e101c01d
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-17 01:58:38 -0300

    Passo 18

--- script.js
+++ script.js
@@ -6,20 +6,11 @@ const button = document.getElementById('criar-carta');
 const input = document.getElementById('carta-texto');
 const result = document.getElementById('carta-gerada');
 const span = document.getElementsByTagName('span');
+let counter = 0;
 
 function random(array) {
   const randomIndex = Math.floor(Math.random() * array.length);
   return randomIndex;
-}
-
-function checkRepeat() {
-  for (let i = 0; i < span.length; i += 1) {
-    for (let j = 1; j < span.length; j += 1) {
-      if (span[i].classList === span[j].classList) {
-        randomClass();
-      }
-    }
-  }
 }
 
 function randomClass() {
@@ -29,6 +20,16 @@ function randomClass() {
     span[i].classList.add(sizeGroup[random(sizeGroup)]);
     span[i].classList.add(rotationGroup[random(rotationGroup)]);
     span[i].classList.add(inclinationGroup[random(inclinationGroup)]);
+  }
+}
+
+function checkRepeat() {
+  for (let i = 0; i < span.length; i += 1) {
+    for (let j = 1; j < span.length; j += 1) {
+      if (span[i].classList === span[j].classList) {
+        randomClass();
+      }
+    }
   }
 }
 
@@ -50,15 +51,22 @@ function createLetter() {
     } else {
       const breakInput = input.value.match(/\b(\w+)\b/g);
       for (let i = 0; i < breakInput.length; i += 1) {
-        const span = document.createElement('span');
-        span.innerText = breakInput[i];
-        result.appendChild(span);
+        const createSpan = document.createElement('span');
+        createSpan.innerText = breakInput[i];
+        result.appendChild(createSpan);
         randomClass();
         checkRepeat();
         clickRandom();
+        countWords();
       }
     }
   });
+}
+
+function countWords() {
+  const counterSpace = document.getElementById('carta-contador');
+  counter = span.length;
+  counterSpace.innerText = `${counter} palavras.`;
 }
 
 createLetter();
