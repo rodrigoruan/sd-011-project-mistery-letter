c29877b266adee7aa467a8146c4c7232c8871ff8
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-18 04:27:13 -0300

    adiciona classe aleatoria para cada palavra criada

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const tagP = document.querySelector('#carta-gerada');
 const botao = document.querySelector('#criar-carta');
+const classes = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+
 botao.addEventListener('click', () => {
   const frase = document.querySelector('#carta-texto').value;
   const palavras = frase.split(' ');
@@ -8,11 +10,13 @@ botao.addEventListener('click', () => {
     if (palavras[0] === '') {
       tagP.innerHTML = 'Por favor, digite o conteúdo da carta.';
     } else {
+      const aleatory = Math.floor(Math.random() * 10);
+      const aleatory2 = Math.floor(Math.random() * 10);
       const makeSpan = document.createElement('span');
       tagP.appendChild(makeSpan);
       makeSpan.innerHTML = palavras[index];
+      makeSpan.classList.add(classes[aleatory]);
+      makeSpan.classList.add(classes[aleatory2]);
     }
   }
-  // console.log(frase);
-  // console.log(palavras);
 });
