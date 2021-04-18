0c09e8015c7309daf8354488a31defaeefaeafd4
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-18 03:19:51 -0300

    adiciona mensagem na tela quando o input estiver vazio ou preenchido com espacos

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const botao = document.querySelector('#criar-carta');
 botao.addEventListener('click', () => {
   const frase = document.querySelector('#carta-texto').value;
   const palavras = frase.split(' ');
+  console.log(palavras);
   tagP.innerHTML = '';
   for (let index = 0; index < palavras.length; index += 1) {
-    const makeSpan = document.createElement('span');
-    tagP.appendChild(makeSpan);
-    makeSpan.innerHTML = palavras[index];
+    if (palavras[0] === '') {
+      tagP.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    } else {
+      const makeSpan = document.createElement('span');
+      tagP.appendChild(makeSpan);
+      makeSpan.innerHTML = palavras[index];
+    }
   }
   // console.log(frase);
   // console.log(palavras);
