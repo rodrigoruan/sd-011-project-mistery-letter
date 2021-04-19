afdf8fd74197e962c9b33fe697678a7c8ae90abc
Alexandre <naitwa.alexandre@gmail.com>
2021-04-19 11:53:55 -0300

    18

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const input = document.getElementById('carta-texto');
 const p = document.getElementById('carta-gerada');
 const button = document.getElementById('criar-carta');
+const countLetter = document.getElementById('carta-contador');
 
 button.addEventListener('click', () => {
+  let count = 0;
   if (input.value.trim() === '') {
     const span = document.createElement('span');
     span.append('Por favor, digite o conteúdo da carta.');
@@ -11,11 +13,13 @@ button.addEventListener('click', () => {
   } else {
     const splitedStr = input.value.split(' ');
     for (let i = 0; i < splitedStr.length; i += 1) {
+      count += 1;
       const span = document.createElement('span');
       span.append(splitedStr[i]);
       p.appendChild(span);
     }
   }
+  countLetter.innerHTML = count;
 });
 
 input.addEventListener('change', () => {
