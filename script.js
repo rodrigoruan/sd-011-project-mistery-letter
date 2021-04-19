354cfab7fd6dbd5f4c894df0d2dba83ce96a3f82
KaioHPs <kaiohpsalternativo@gmail.com>
2021-04-18 22:33:48 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ const letterInput = document.querySelector('#carta-texto');
 const letterBtn = document.querySelector('#criar-carta');
 const letterFinalParagraph = document.querySelector('#carta-gerada');
 
-function splitString() {
-  letterBtn.addEventListener('click', () => {
-    letterFinalParagraph.innerHTML = '';
-    const words = letterInput.value.split(' ');
-    for (let index = 0; index < words.length; index += 1) {
-      const word = words[index];
+function splitString2(string) {
+  const words = string.split(' ');
+  for (let index = 0; index < words.length; index += 1) {
+    const word = words[index];
+    if (word.trim().length) {
       const spanWord = document.createElement('span');
       spanWord.innerText = word;
       letterFinalParagraph.appendChild(spanWord);
+    }
+  }
+}
+
+function splitString() {
+  letterBtn.addEventListener('click', () => {
+    const string = letterInput.value;
+    if (!string || !string.trim().length) {
+      alert('Por favor, digite o conteúdo da carta.');
+    } else {
+      letterFinalParagraph.innerHTML = '';
+      splitString2(string);
     }
   });
 }
