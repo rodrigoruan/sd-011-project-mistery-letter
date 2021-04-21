2ff2215022af111b4df4e4d002f8682ba2ee96e9
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-21 14:22:27 -0300

    sdfklç

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ createLetter.addEventListener('click', function(){
     let mostrarPalavras = document.getElementById('carta-gerada');
     let frase = document.getElementById('carta-texto').value;
     let palavrasFrase = frase.split(' ');
-
     for(let i=0; i<palavrasFrase.length;i++){
         let spanFrase = document.createElement('span');
         spanFrase.innerText = palavrasFrase[i];
         mostrarPalavras.appendChild(spanFrase);
     }
-   
     //Essa será minha carta
 });
