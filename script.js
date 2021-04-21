8c9aaa79610c1d36027aa2e74522ec02444a3568
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-21 14:37:56 -0300

    FKLÇSDG

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 let createLetter = document.getElementById('criar-carta');
 createLetter.addEventListener('click', function(){
     let mostrarPalavras = document.getElementById('carta-gerada');
+    mostrarPalavras.innerHTML = '';
     let frase = document.getElementById('carta-texto').value;
     let palavrasFrase = frase.split(' ');
     for(let i=0; i<palavrasFrase.length;i++){
         let spanFrase = document.createElement('span');
         spanFrase.innerText = palavrasFrase[i];
+        spanFrase.innerText += ' ';
         mostrarPalavras.appendChild(spanFrase);
     }
+    console.log(mostrarPalavras);
     //Essa será minha carta
 });
