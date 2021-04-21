56147fb4e2a45f65b6bee18e3b24a5932c7800fc
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-21 14:17:37 -0300

    Exercicio 3 - filhas span pai p

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let createLetter = document.getElementById('criar-carta');
 createLetter.addEventListener('click', function(){
     let mostrarPalavras = document.getElementById('carta-gerada');
     let frase = document.getElementById('carta-texto').value;
-    let spanFrase = document.createElement('span');
     let palavrasFrase = frase.split(' ');
-    console.log('Palavras como normal: '+palavrasFrase);
+
     for(let i=0; i<palavrasFrase.length;i++){
-        spanFrase[i]=palavrasFrase[i];
-        mostrarPalavras.appendChild(spanFrase[i]);
+        let spanFrase = document.createElement('span');
+        spanFrase.innerText = palavrasFrase[i];
+        mostrarPalavras.appendChild(spanFrase);
     }
-    console.log('Palavras como span: ' + spanFrase[0]);
+   
     //Essa será minha carta
 });
