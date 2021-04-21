1375d588cb3e44e207b0d89ac1d26a02a1bbbf4a
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-21 15:11:14 -0300

    Exercicio 5

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ createLetter.addEventListener('click', function(){
     let mostrarPalavras = document.getElementById('carta-gerada');
     mostrarPalavras.innerHTML = '';
     let frase = document.getElementById('carta-texto').value;
-    let palavrasFrase = frase.split(' ');
-    for(let i=0; i<palavrasFrase.length;i++){
-        let spanFrase = document.createElement('span');
-        spanFrase.innerText = palavrasFrase[i];
-        mostrarPalavras.appendChild(spanFrase);
+    if (frase=='' || !frase.trim()){
+        mostrarPalavras.innerText = 'Por favor, digite o conteúdo da carta.';
+    }else{
+        let palavrasFrase = frase.split(' ');
+        for(let i=0; i<palavrasFrase.length;i++){
+            let spanFrase = document.createElement('span');
+            spanFrase.innerText = palavrasFrase[i];
+            mostrarPalavras.appendChild(spanFrase);
+        }
     }
-    console.log(mostrarPalavras);
     //Esta será minha carta
 });
