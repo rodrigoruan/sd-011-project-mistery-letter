33caca39c064e43d6852e40bfd313faf39bf68de
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-04-25 14:25:05 -0300

    100% requisitos e linter - sem estilização

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const styleClassGroup = ['newspaper','magazine1', 'magazine2'];
-const sizeClassGroup = ['medium','big','reallybig'];
-const rotationClassGroup = ['rotateleft','rotateright'];
-const skewnessClassGroup = ['skewleft','skewright'];
+const styleClass = ['newspaper', 'magazine1', 'magazine2'];
+const sizeClass = ['medium', 'big', 'reallybig'];
+const rotClass = ['rotateleft', 'rotateright'];
+const skewClass = ['skewleft', 'skewright'];
 const textButton = document.getElementById('criar-carta');
 const generatedLetter = document.getElementById('carta-gerada');
+const letterCounter = document.getElementById('carta-contador');
 
-
-function randomClassGroup() {
-  let styleIndex = Math.floor(Math.random() * 3); 
-  let sizeIndex = (Math.random()>0.5)? 1 : 0; 
-  let rotationIndex = (Math.random()>0.5)? 1 : 0;
-  let skewnessIndex = (Math.random()>0.5)? 1 : 0;
-  const classList = [styleClassGroup[styleIndex], sizeClassGroup[sizeIndex], rotationClassGroup[rotationIndex], skewnessClassGroup[skewnessIndex]];
+function randClass() {
+  const style = Math.floor(Math.random() * 3);
+  const size = (Math.random() > 0.5) ? 1 : 0;
+  const rotation = (Math.random() > 0.5) ? 1 : 0;
+  const skewness = (Math.random() > 0.5) ? 1 : 0;
+  const classList = [styleClass[style], sizeClass[size], rotClass[rotation], skewClass[skewness]];
   return classList;
 }
-
 
 function generateLetter() {
   textButton.addEventListener('click', () => {
@@ -25,12 +24,13 @@ function generateLetter() {
     } else {
       const splitText = textInput.split(' ');
       for (let index = 0; index < splitText.length; index += 1) {
-        const letterContent = document.createElement('span');
-        letterContent.innerText = splitText[index];
-        letterContent.classList.add(randomClassGroup()[0],randomClassGroup()[1],randomClassGroup()[2], randomClassGroup()[3]);
-        generatedLetter.appendChild(letterContent);
+        const letterCont = document.createElement('span');
+        letterCont.innerText = splitText[index];
+        letterCont.classList.add(randClass()[0], randClass()[1], randClass()[2], randClass()[3]);
+        generatedLetter.appendChild(letterCont);
       }
     }
+    letterCounter.innerText = generatedLetter.childElementCount;
   });
 }
 generateLetter();
