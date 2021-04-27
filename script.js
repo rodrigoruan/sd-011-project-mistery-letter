9baa768761377d5058edde95d76b6a2ad027088c
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-27 12:16:20 -0300

    Adiciona o requisito 5

--- script.js
+++ script.js
@@ -8,12 +8,17 @@ function getInputText() {
 document.getElementById('criar-carta').addEventListener('click', () => {
   const text = getInputText();
   const generateLetter = document.getElementById('carta-gerada');
+  const inputText = document.getElementById('carta-texto');
 
   generateLetter.innerHTML = '';
 
-  for (let index = 0; index < text.length; index += 1) {
-    const createSpan = document.createElement('span');
-    createSpan.innerText = text[index];
-    generateLetter.appendChild(createSpan);
+  if (inputText.value === '' || inputText.value === ' ') {
+    generateLetter.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    for (let index = 0; index < text.length; index += 1) {
+      const createSpan = document.createElement('span');
+      createSpan.innerText = text[index];
+      generateLetter.appendChild(createSpan);
+    }
   }
 });
