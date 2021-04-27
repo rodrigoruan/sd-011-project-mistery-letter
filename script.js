00316682aa2a71646b68f9e3df743dd8e0b846c6
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-27 13:43:06 -0300

    Adiciona o requisito 16 e 17

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ document.getElementById('criar-carta').addEventListener('click', () => {
     }
   }
 });
+
+const styles = {
+  style: ['newspaper', 'magazine1', 'magazine2'],
+  size: ['medium', 'big', 'reallybig'],
+  rotation: ['rotateleft', 'rotateright'],
+  slope: ['skewleft', 'skewright'],
+
+};
+
+document.getElementById('criar-carta').addEventListener('click', () => {
+  const span = document.getElementsByTagName('span');
+
+  for (let index = 0; index < span.length; index += 1) {
+    span[index].classList.add(styles.style[Math.floor(Math.random() * styles.style.length)]);
+    span[index].classList.add(styles.size[Math.floor(Math.random() * styles.size.length)]);
+    span[index].classList.add(styles.rotation[Math.floor(Math.random() * styles.rotation.length)]);
+    span[index].classList.add(styles.slope[Math.floor(Math.random() * styles.slope.length)]);
+  }
+});
