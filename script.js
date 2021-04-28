032c2c89efdcb96b4df833f34ab3103c24204fcd
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-28 11:09:08 -0300

    Requisito 5 - mensagem de conteudo digitado

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ const showCard = document.getElementById('carta-gerada');
 
 const getButtonCard = document.getElementById('criar-carta');
 
+// Gerar carta
 getButtonCard.addEventListener('click', () => {
   showCard.innerHTML = inputText.value;
 });
 
-
-
-
+// Manter texto no input
 function saveSotorage() {
   const inputCarta = document.getElementById('carta-texto').value;
   localStorage.setItem("carta", inputCarta);
   localStorage.getItem("carta");
   }
+
+
+function checkTextInput() {
+  if(inputText.value === null || inputText.value === ' ') {
+    showCard.innerHTML = 'Por favor, digite o conteúdo da carta';
+  }
+}
 
 
 window.onload = function() {
