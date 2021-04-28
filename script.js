c7c2e462108c87e2ad6bb851216536045dd3adf2
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-28 11:02:15 -0300

    Corrige nome de variaveis e requisito 3

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const inputText = document.getElementById('carta-texto');
 
-const Showcarta = document.getElementById('carta-gerada');
+const showCard = document.getElementById('carta-gerada');
 
-const buttonCarta = document.getElementById('criar-carta');
+const getButtonCard = document.getElementById('criar-carta');
+
+getButtonCard.addEventListener('click', () => {
+  showCard.innerHTML = inputText.value;
+});
+
+
+
 
 function saveSotorage() {
   const inputCarta = document.getElementById('carta-texto').value;
