9d4c53e2b85ff961f0b087e27cea8a772dde3f33
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-28 10:53:58 -0300

    Requisito 3 - cria botao para gerar carta

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const inputText = document.getElementById('carta-texto');
 const Showcarta = document.getElementById('carta-gerada');
 
 const buttonCarta = document.getElementById('criar-carta');
+
+function saveSotorage() {
+  const inputCarta = document.getElementById('carta-texto').value;
+  localStorage.setItem("carta", inputCarta);
+  localStorage.getItem("carta");
+  }
+
+
+window.onload = function() {
+  saveSotorage();
+}
