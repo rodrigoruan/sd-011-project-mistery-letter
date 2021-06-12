4abbb9f43b10b448fa09210334ba04ce49874a65
IsleneGomes <islenegomes@gmail.com>
2021-06-12 14:53:36 -0300

    Requisito Finais

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ function DivideTexto() {
 }
 
 botao.addEventListener('click', () => {
-// constante que traz a função para o eventlistener
+  // constante que traz a função para o eventlistener
   const palavras = DivideTexto();
   const paragrafo = document.getElementById('carta-gerada');
   // limpa o parágrafo para não acumular palavras
   paragrafo.innerHTML = '';
   // condicional se o valor do input for vazio ou espaço em branco emite o texto
   if (input.value === ' ' || input.value === '') {
-    paragrafo.innerText = 'Por favor, digite o conteúdo da carta.';
-    } else {
+    paragrafo.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
     for (let index = 0; index < palavras.length; index += 1) {
       const span = document.createElement('span');
       span.innerText = palavras[index];
@@ -37,16 +37,17 @@ const estilos = {
 botao.addEventListener('click', () => {
   const span = document.getElementsByTagName('span');
   for (let index = 0; index < span.length; index += 1) {
-    span[index].classList.add(estilos.style[Math.floor(Math.random() * estilos.style.length)]);
-    span[index].classList.add(estilos.size[Math.floor(Math.random() * estilos.size.length)]);
-    span[index].classList
-      .add(estilos.rotation[Math.floor(Math.random() * estilos.rotation.length)]);
-    span[index].classList.add(estilos.slope[Math.floor(Math.random() * estilos.slope.length)]);
+    span[index].classList.add(
+      estilos.style[Math.floor(Math.random() * estilos.style.length)]
+    );
+    span[index].classList.add(
+      estilos.size[Math.floor(Math.random() * estilos.size.length)]
+    );
+    span[index].classList.add(
+      estilos.rotation[Math.floor(Math.random() * estilos.rotation.length)]
+    );
+    span[index].classList.add(
+      estilos.slope[Math.floor(Math.random() * estilos.slope.length)]
+    );
   }
-});
-
-// Adiciona contador de palavras da carta
-botao.addEventListener('click', () => {
-  const totalSpan = document.getElementsByTagName('span').length;
-  document.getElementById('carta-contador').innerText = totalSpan;
 });
