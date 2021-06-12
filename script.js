4bb83f654036fa3b1e36e52019119573a576c929
IsleneGomes <islenegomes@gmail.com>
2021-06-12 13:43:31 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-const input = document.getElementById('carta-texto').value;
+const input = document.getElementById('carta-texto');
 const botao = document.getElementById('criar-carta');
-const paragrafo = document.getElementById('carta-gerada');
+
+function DivideTexto() {
+  const input = document.getElementById('carta-texto');
+  const palavras = input.value.split(' ');
+  return palavras;
+}
 
 botao.addEventListener('click', () => {
-  if (input !== undefined) {
-    let palavras = [];
-    palavras.push(input.split(' '));
-    let span = document.createElement('span');
-    palavras = paragrafo.appendChild(span);
-    for (let index = 0; index <= palavras.length; index += 1) {
-      palavras[index] = span[index].value;
-    }
+  const palavras = DivideTexto();
+  const paragrafo = document.getElementById('carta-gerada');
+  paragrafo.innerHTML = ' ';
+  if (palavras === '' || palavras === ' ') {
+    paragrafo.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
-    paragrafo.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  for (let index = 0; index < palavras.length; index += 1) {
+      const span = document.createElement('span');
+      span.innerText = palavras[index];
+      paragrafo.appendChild(span);
   }
-});
+}});
